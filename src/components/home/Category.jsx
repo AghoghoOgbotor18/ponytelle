55d5a4e1dda22106b2acb6561d7e1afa998c534a
Aghogho Ogbotor <sarahogbotor27@gmail.com>
2026-02-28 15:19:39 +0100

    style: add subtle animation to category cards

--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { delay, easeOut, motion } from "framer-motion";
 
 const Category = () => {
+
+    const containerVariants = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.3,
+                delayChildren: 0.2
+            }
+        },
+    }
+
+    const itemVariants ={
+        hidden: { opacity: 0, y:20 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.5,
+                ease: "easeOut"
+            }
+        }
+    }
+
     const categories = [
         {
             image: "https://res.cloudinary.com/dysyzuviq/image/upload/v1770454834/str_wig_yzixaf.jpg",
@@ -30,27 +55,27 @@ const Category = () => {
         navigate(`/shop?category=${slug}`)
     }
 
-  return (
-    <section className='container mx-auto my-18 flex flex-col gap-8 justify-center items-center'>
-        <div className='text-center my-3'>
-            <h2 className='font-black text-4xl mb-2'>Our Collections</h2>
-            <p className='text-md'>Check out our Ponytail Collections</p>
-        </div>
-        <div className='flex justify-center'>
-            <div className='md:w-[85%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-9 max-sm:gap-18'>
-            {categories.map((c, id) => (
-            <div key={id} className='relative overflow-hidden max-sm:rounded-[35%] sm:rounded-full' onClick={() => handleClick(c.slug)}>
-                <img src={c.image} alt={c.name} className='w-90 md:[w-65] h-90'/>
-                <div className='absolute inset-0 bg-linear-to-t from-black/80 hover:from-[#281a17] hover:via-[#3a2621]/30 active:from-[#281a17] active:via-[#3a2621]/30 to-transparent cursor-pointer'></div>
-                <p className='absolute bottom-10 max-sm:left-28 sm:left-18 md:left-12 text-white text-lg font-semibold z-10'>
-                    {c.name}
-                </p>
+    return (
+        <section className='container mx-auto my-18 flex flex-col gap-8 justify-center items-center'>
+            <div className='text-center my-3'>
+                <h2 className='font-black text-4xl mb-2'>Our Collections</h2>
+                <p className='text-md'>Check out our Ponytail Collections</p>
             </div>
-            ))}
-        </div>
-        </div>
-    </section>
-  )
+            <motion.div className='flex justify-center'  variants={containerVariants} initial="hidden" whileInView="visible" viewport={{once: true, amount: 0.3}}>
+                <div className='md:w-[85%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-9 max-sm:gap-18'>
+                    {categories.map((c, id) => (
+                    <motion.div key={id} className='relative overflow-hidden max-sm:rounded-[35%] sm:rounded-full hover:-translate-y-2 duration-500 flex justify-center' variants={itemVariants} onClick={() => handleClick(c.slug)}>
+                        <img src={c.image} alt={c.name} className='w-90 md:[w-65] h-90'/>
+                        <div className='absolute inset-0 bg-linear-to-t from-black/80 hover:from-[#281a17] hover:via-[#3a2621]/30 active:from-[#281a17] active:via-[#3a2621]/30 to-transparent cursor-pointer'></div>
+                        <p className='absolute bottom-10 flex justify-center items-center text-white text-lg font-semibold z-10 text-center'>
+                            {c.name}
+                        </p>
+                    </motion.div>
+                    ))}
+                </div>
+            </motion.div>
+        </section>
+    )
 }
 
 export default Category
