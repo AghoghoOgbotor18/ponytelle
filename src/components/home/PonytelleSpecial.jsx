e478bfbbed991829a5fd3a65eb3a046b75216573
Aghogho Ogbotor <sarahogbotor27@gmail.com>
2026-03-08 10:39:43 +0100

    fix: improve mobile alignment for Ponytelle Specials carousel cards

--- src/components/home/PonytelleSpecial.jsx
+++ src/components/home/PonytelleSpecial.jsx
@@ -89,10 +89,10 @@ const PonytelleSpecial = () => {
             {/* Carousel */}
             <div
                 ref={scrollRef}
-                className="flex gap-4 overflow-x-auto scroll-smooth scrollbar-hide pb-4"
+                className="flex gap-4 overflow-x-auto scroll-smooth scrollbar-hide pb-4 px-6"
             >
                 {bestSellers.map((product) => (
-                    <div key={product.id} className="w-[48%] md:w-[200px] flex-shrink-0">
+                    <div key={product.id} className="w-[52%] md:w-[220px] flex-shrink-0">
                         <ProductCard product={product} variant="special" />
                     </div>
                 ))}
